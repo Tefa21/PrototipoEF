1c4ecd306857b1b655a49107c807bc65881ed8f5
Tefa21 <stmario.campos@gmail.com>
2020-11-24 12:22:27 -0600

    Mantenimiento de Tipo de Competencias

--- ModuloHRM/Capa_Vista/Vista Mantenimientos/Tipo Competencia/frmTipoCompetencia.cs
+++ ModuloHRM/Capa_Vista/Vista Mantenimientos/Tipo Competencia/frmTipoCompetencia.cs
@@ -33,7 +33,46 @@ namespace Capa_Vista.Vista_Mantenimientos.Tipo_Competencia
 
         private void NavegadorCompetencias_Load(object sender, EventArgs e)
         {
-            
+            List<string> CamposTabla = new List<string>();
+            List<Control> lista = new List<Control>();
+            //el numero de aplicacion se debe cambiar por el numero asignado en la base de datos 
+            NavegadorCompetencias.aplicacion = 1;
+            //banco se debe cambiar por la tabla a la que se quiere hacer el mantenimiento
+            NavegadorCompetencias.tbl = "tipo_competencia";
+            //estado_banco se debe cambiar por el estado de la tabla a la que se desea hacer mantenimiento
+            NavegadorCompetencias.campoEstado = "estado_competencia";
+            NavegadorCompetencias.MDIformulario = FormularioPadre;
+            foreach (Control C in this.Controls)
+            {
+                if ((C.Tag != null) && (!C.Tag.ToString().Equals("")))
+                {
+                    if (C is TextBox)
+                    {
+                        lista.Add(C);
+
+                    }
+                    else if (C is ComboBox)
+                    {
+                        lista.Add(C);
+
+                    }
+                    else if (C is DateTimePicker)
+                    {
+                        lista.Add(C);
+                    }
+                }
+            }
+            NavegadorCompetencias.control = lista;
+            NavegadorCompetencias.formulario = this;
+            //el dataGridView1 se debe cambiar por el que se tiene en el formulario
+            NavegadorCompetencias.DatosActualizar = dgvCompetencias;
+            NavegadorCompetencias.procActualizarData();
+            NavegadorCompetencias.procCargar();
+            //en la variable ayuda ruta debe colocar la carpeta y el archivo.chm de su proyecto de ayuda
+            NavegadorCompetencias.ayudaRuta = "Ayudas_HRM/Ayudas_Modulo_HRM.chm";
+            //en ruta debe colocar la pagina html que quiere que se muestre cuendo se presione el boton ayuda
+            NavegadorCompetencias.ruta = "pg_0014.htm";
+
         }
 
         private void rbtnActivo_CheckedChanged(object sender, EventArgs e)
